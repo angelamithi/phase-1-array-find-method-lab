a611cfa0aa8f16961629fd7c0503d140b5ff1746
+ <+>
2023-09-23 19:48:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+//function findWinningGame(game) {
+  //  return game.result === "W";
+//}
+
 function superbowlWin(record) {
-    const winningYear = record.find(entry => entry.result === "W");
-    return winningYear ? winningYear.year : undefined;
-  }
+    let winningGame = record.find(game => game.result==="W");
+    if (winningGame)
+    {
+       return winningGame.year;
+    }
+    else {
+        return undefined;
+    }
+   }
